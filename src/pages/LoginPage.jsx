b1e2a98ee6a70bca3b83bd7f58fd8e62c1f39351
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-11-25 14:42:28 -0500

    mod: go to movies if already logged in

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,17 @@
 import LoginForm from "@components/LoginForm";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
+  const user = localStorage.getItem("token");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/movies");
+    }
+  }, [user]);
+
   return (
     <section className="h-screen flex items-center justify-center">
       <LoginForm />
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,6 +1,16 @@
 import RegisterForm from "@components/RegisterForm";
+import { useNavigate } from "react-router-dom";
 
 function RegisterPage() {
+  const user = localStorage.getItem("token");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/movies");
+    }
+  }, [user]);
+
   return (
     <section className="h-screen flex items-center justify-center">
       <RegisterForm />
